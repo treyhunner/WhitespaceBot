f38da1b168ff834ca660293a706f5b74d70ae12f
Trey Hunner <treyhunner@gmail.com>
2011-12-21 13:18:41 -0800

    Prompt user for credentials when left unspecified

--- settings.py
+++ settings.py
@@ -1,4 +1,10 @@
+import getpass
+
 # Put your User/Pass in here!
 
 username='YourUsername'
 password='YourPassword'
+
+if username == 'YourUsername' and password == 'YourPassword':
+    username = raw_input('Github Username: ')
+    password = getpass.getpass('Github Password: ')
